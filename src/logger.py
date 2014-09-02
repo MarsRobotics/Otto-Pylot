75131530d677b9ab48fb4f3459358cd818fde01c
Taylor Nightingale <nighting47@gmail.com>
2014-09-01 23:27:36 -0700

    Updated the logger, and added documentation on how to use it.

--- src/logger.py
+++ src/logger.py
@@ -1,30 +1,65 @@
 ##
 # Logger
-# TODO: talk about logger here
-# The logger's name is a period-separated hierarchical value. eg. "core.logger" follows the file structure
+#
+# The logger can log DEBUG, INFO, WARN, ERROR, and CRITICAL messages through
+# functions of the same (lower case) name. All messages will be printed to
+# the log, but only INFO, WARN, ERROR, and CRITICAL will be printed to the
+# console. The root logger can be accessed through a method call on logger
+# (e.g. logger.debug("...")).
+#
+# Your own logger should be used for each file to denote where the logs come from.
+# This can be achieved through proper naming of the logger when initializing it in
+# the file. A log for file transport/main should be named "transport.main". The
+# method of creating a logger is as follows:
+#
+#       myLogger = logging.getLogger('transport.main')
+#
+# The logger can then be used as above (e.g. myLogger.debug("..")).
+#
+# File logs are in the format:
+# 10/22/2014 07:19:45 root         INFO     Booting up...
+# 10/22/2014 07:22:34 robot.core   DEBUG    I just can't do it cap'n. I just don't have the power.
+#
+# Console logs are in the format:
+# root        : INFO     Booting up...
+# robot.core  : WARNING  I will probably lose power soon.
 ##
 __author__ = 'Taylor Nightingale'
 
 import logging
 
-LOGGING_FILE_NAME   =  'otto-pylot-%(asctime)s.log'
-LOGGING_FORMAT      =  '%(levelname)s [%(asctime)s]: %(message)s' ## ex. WARN [12/12/2010 11:46:36 AM]: I moved.
-LOGGING_DATE_FORMAT =  '%m/%d/%Y-%I:%M:%S%p' ## ex. 12/12/2010 11:46:36 AM
-LOGGING_FILE_MODE   =  'w' ## write only
-LOGGING_BASE_LEVEL  =  logging.WARN
+## Constants ##
+LOGGING_FILE_NAME = '../logs/otto-pylot.log'
+LOGGING_FORMAT = '%(asctime)s %(name)-12s %(levelname)-8s - %(message)s'
+LOGGING_DATE_FORMAT = '%m/%d/%Y %I:%M:%S %p'
+CONSOLE_FORMAT = '%(name)-12s: %(levelname)-8s %(message)s'
+LOGGING_FILE_MODE = 'w' # write only
 
-def setUp():
-    logging.basicConfig(filename =  LOGGING_FILE_NAME,
-                        format   =  LOGGING_FORMAT,
-                        datefmt  =  LOGGING_DATE_FORMAT,
-                        filemode =  LOGGING_FILE_MODE,
-                        level    =  LOGGING_BASE_LEVEL
-                        )
+# set the default options for the logger
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=LOGGING_FORMAT,
+    datefmt=LOGGING_DATE_FORMAT,
+    filename=LOGGING_FILE_NAME,
+    filemode=LOGGING_FILE_MODE)
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+
+# set a format for console use
+formatter = logging.Formatter(CONSOLE_FORMAT)
+
+# tell the handler to use the console format
+console.setFormatter(formatter)
+
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
 
 ## Testing stuff
-setUp()
-logging.warn("")
-
-
-
+logging.debug('debug message')
+logging.info('info message')
+logging.warn('warn message')
+logging.error('error message')
+logging.critical('critical message')
 
